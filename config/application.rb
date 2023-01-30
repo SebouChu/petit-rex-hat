32c1ad143b4c68153bf4b8aa0448f1b4061b85f3
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-01-30 23:12:54 +0100

    devise user

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,15 @@ module PetitRexHat
     config.time_zone = "Europe/Paris"
     config.i18n.default_locale = :fr
 
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address: "smtp-relay.sendinblue.com",
+        port: 587,
+        user_name: ENV['SENDINBLUE_SMTP_USER'],
+        password: ENV['SENDINBLUE_SMTP_PASSWORD'],
+        authentication: :plain
+    }
+
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
