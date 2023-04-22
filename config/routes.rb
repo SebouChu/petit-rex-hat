3cf2432953a55d5b294340e86e99d922f21a1be9
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-04-22 16:14:52 +0200

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     get "/users/cancel" => "users/registrations#cancel", as: :cancel_user_registration
     get "/users/sign_up" => "users/registrations#new", as: :new_user_registration
     get "/users/edit" => "users/registrations#edit", as: :edit_user_registration
-    patch "/users" => "users/registrations#update", as: :user_registration
+    post "/users" => "users/registrations#create", as: :user_registration
+    patch "/users" => "users/registrations#update", as: ''
     put "/users" => "users/registrations#update", as: ''
     delete "/users" => "users/registrations#destroy", as: ''
   end
