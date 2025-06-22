f04da0c21f99b36a7b429f3786b5738ea14de16d
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-06-22 11:47:49 +0200

    remove registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   validates :nickname, uniqueness: true, allow_blank: true
 
